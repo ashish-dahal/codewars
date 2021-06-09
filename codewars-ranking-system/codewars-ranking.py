14c0b822616872ff33ed6937eb3f2edfebde9782
Ashish Dahal <ashishdahal1997@gmail.com>
2021-06-09 21:04:20 +0545

    Refactored solution for codewars ranking problem

--- codewars-ranking-system/codewars-ranking.py
+++ codewars-ranking-system/codewars-ranking.py
@@ -1,29 +1,31 @@
 RANK = (-8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8)
 
+
 class User:
     def __init__(self):
-        self.rank = RANK[0]
+        self.rank = -8
         self.progress = 0
-        self.rank_index = 0
-        
+
     def inc_progress(self, activity_rank):
-        # Check is activity rank is valid
+        # Check if activity rank is valid
         if activity_rank not in RANK:
             raise Exception('Sorry! The activity rank is invalid.')
+
+        rank_index = RANK.index(self.rank)
         activity_rank_index = RANK.index(activity_rank)
-        
+
         # Calculate the progress
-        if activity_rank_index == self.rank_index:
+        if activity_rank == self.rank:
             self.progress += 3
-        elif activity_rank_index == self.rank_index - 1:
+        elif activity_rank_index == rank_index - 1:
             self.progress += 1
-        elif activity_rank_index > self.rank_index:
-            self.progress += 10*(activity_rank_index - self.rank_index)**2
-            
+        elif activity_rank > self.rank:
+            self.progress += 10*(activity_rank_index - rank_index)**2
+
         # Assign the rank based on progress progress
-        if self.progress >= 100 and self.rank_index < 15:
-                self.rank_index += self.progress//100
-                self.rank = RANK[self.rank_index] if self.rank_index <= 15 else 8
-                self.progress %= 100
+        if self.progress >= 100 and self.rank < 8:
+            rank_index += self.progress//100
+            self.rank = RANK[rank_index] if rank_index <= 15 else 8
+            self.progress %= 100
         if self.rank == 8:
             self.progress = 0
